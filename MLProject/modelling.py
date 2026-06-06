9e18094064f25181d4f2ef83dae042a374530e57
ayughaniyatur <ayughaniyatur05@gmail.com>
2026-06-06 18:21:43 +0700

    Initial MLflow CI workflow

---
+++ MLProject/modelling.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# Load Data
+df = pd.read_csv("customer_churn_processed.csv")
+
+TARGET_COLUMN = "Churn"
+
+X = df.drop(columns=[TARGET_COLUMN])
+y = df[TARGET_COLUMN]
+
+# Split Data
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+# Training
+with mlflow.start_run():
+    model = RandomForestClassifier(n_estimators=200, random_state=42)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+
+    mlflow.log_param("model_type", "RandomForestClassifier")
+    mlflow.log_param("n_estimators", 200)
+
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_metric("precision", precision)
+    mlflow.log_metric("recall", recall)
+    mlflow.log_metric("f1_score", f1)
+
+    mlflow.sklearn.log_model(sk_model=model, artifact_path="model")
+
+    print("Training selesai")
